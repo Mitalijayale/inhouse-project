0af39da4bdadc8f5a0b62a4f1461b49a6907e470
MJ <madhav3jadhav@gmail.com>
2024-02-01 21:52:10 +0530

    committee form basic structure done, responsiveness few changes needed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import './App.css';
 import Header from "./components/Header/Header.jsx";
 import AddStaffDetails from "./components/AddStaffDetails/AddStaffDetails.jsx";
 import CreateCommitteeForm from "./components/CreateCommitteeForm/CreateCommitteeForm.jsx";
+import AcademicYearSelection from "./components/MiniComponents/AcademicYearSelection/AcademicYearSelection.jsx";
 
-const com=true;
+const com = true;
 
 function App() {
-  return(<div>
-    <Header/>
-    {com?<CreateCommitteeForm/>:<AddStaffDetails/>}
-  </div>);
+  return (
+    <div>
+      <Header />
+      {com ? (
+        <>
+          <AcademicYearSelection />
+          <CreateCommitteeForm />
+        </>
+      ) : (
+        <AddStaffDetails />
+      )}
+    </div>
+  );
 }
+
 
 export default App;
--- src/components/CreateCommitteeForm/CreateCommitteeForm.jsx
+++ src/components/CreateCommitteeForm/CreateCommitteeForm.jsx
@@ -1,22 +1,27 @@
 import React from "react";
-import AcademicYearSelection from "../MiniComponents/AcademicYearSelection/AcademicYearSelection.jsx";
 import "./CreateCommitteeForm.css";
 
 function CreateCommitteeForm() {
   return (
     <div className="container">
-      <AcademicYearSelection />
       <h2 className="headText">Create new committee</h2>
-      <div className="committeeForm">
-        <div id="row1">
-          <label id="comLab">Name of the committee</label>
-          <label id="catLab">Category</label>
-          <br />
-        </div>
-
-        <div id="row2">
-          <input type="text" placeholder="Committee name" />
-          <input type="text" placeholder="Academic Committee" />
+      <form className="committeeForm">
+        <div className="rowMain">
+          <div id="row1">
+            <label id="comLab">Name of the committee</label>
+            <br />
+            <input
+              type="text"
+              placeholder={"\u00A0\u00A0\u00A0\u00A0 Committee name"}
+            />
+          </div>
+          <div id="row2">
+            <label id="catLab">Category</label>
+            <input
+              type="text"
+              placeholder={"\u00A0\u00A0\u00A0\u00A0 Academic Committee"}
+            />
+          </div>
         </div>
 
         <div id="row3">
@@ -25,47 +30,57 @@ function CreateCommitteeForm() {
           <textarea></textarea>
         </div>
 
-        <div id="row4">
+        <div id="addRow">
           <label id="memLab">Members</label>
-          <br />
-          <label >Name</label>
-          <label >Designation</label>
         </div>
 
-        <div id="row5">
-          <select>
-            <option>john</option>
-            <option>wick</option>
-            <option>boomer</option>
-          </select>
-          <select>
-            <option>teacher</option>
-            <option>assistant</option>
-            <option>hod</option>
-          </select>
-          <button>&#x1F5D1;</button>
+        <div id="rowMain1">
+          <div id="row4">
+            <label>Name</label>
+            <br />
+            <select>
+              <option hidden>{"\u00A0\u00A0 Select Member"}</option>
+              <option>John Wick</option>
+              <option>Ben Dover</option>
+              <option>Jayson Staton</option>
+            </select>
+          </div>
+
+          <div id="row5">
+            <label>Designation</label>
+            <br />
+            <select>
+              <option hidden>{"\u00A0\u00A0 Select Designation"}</option>
+              <option>Committee Head</option>
+              <option>Marketing</option>
+              <option>Finance</option>
+            </select>
+            <button>&#x1F5D1;</button>
+          </div>
         </div>
 
         <div id="row6">
           <button>New Member</button>
         </div>
 
-        <div id="row7">
-          <label>Upload document if any</label>
-          <br />
+        <div id="row8">
+        <label>Upload document if any</label>
+        <br />
+          <div className="custom-file-input">
+            <input type="file" id="fileInput" className="input-hidden" />
+            <label htmlFor="fileInput">
+            <span>📁 Select File</span>
+            </label>
+          </div>
         </div>
 
-        <div id="row8">
-          <input type="file"></input>
-        </div>
 
         <div id="row9">
-            <button id="btnReset">Reset</button>
-            <button id="btnCancel">Cancel</button>
-            <button id="btnSave">Save</button>
+          <button type="reset" id="btnReset">Reset</button>
+          <button id="btnCancel">Cancel</button>
+          <button id="btnSave">Save</button>
         </div>
-
-      </div>
+      </form>
     </div>
   );
 }
